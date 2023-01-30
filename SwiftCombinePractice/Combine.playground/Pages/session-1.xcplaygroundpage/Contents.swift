7f4f996d929fbcbad2ba693ce71040777732dc91
KantaSwift <kanta-_-soccer.love@docomo.ne.jp>
2023-01-30 16:04:16 +0900

    Publisherについて

--- SwiftCombinePractice/Combine.playground/Pages/session-1.xcplaygroundpage/Contents.swift
+++ SwiftCombinePractice/Combine.playground/Pages/session-1.xcplaygroundpage/Contents.swift
@@ -5,9 +5,10 @@
 // 例①: 文字列の値を渡すイベント
 
 import Combine
+import Foundation
 
 // 決して失敗しないことをNeverで表している
-// イベントを送信するオブジェクト
+// イベントを送信するオブジェクト(※Publisherという)
 let subject = PassthroughSubject<String, Never>()
 
 final class Receiver {
@@ -26,3 +27,51 @@ final class Receiver {
 let receiver = Receiver()
 subject.send("a")
 subject.send("i")
+
+//Publisher
+// ->上記でいうPassthroughSubjectはPublisherの一つ
+// 標準フレームワークでPublisherを生成するものが他にもある
+
+// 例②: NotificationCenter
+
+let myNotification = Notification.Name("MyNotification")
+let publisher = NotificationCenter.default.publisher(for: myNotification)
+
+final class Receiver2 {
+    private var subscriptions = Set<AnyCancellable>()
+    
+    init() {
+        publisher.sink { value in
+            print("Received Value:", value)
+        }
+        .store(in: &subscriptions)
+    }
+}
+
+let receiver2 = Receiver2()
+NotificationCenter.default.post(Notification(name: myNotification))
+// NotificationCenterはpostされるとイベントが発生される
+
+// 例③: Timer
+
+let publisher2 = Timer.publish(every: 1, on: .main, in: .common)
+
+final class Receiver3 {
+    private var subscriptions = Set<AnyCancellable>()
+    
+    init() {
+        publisher2
+            .sink { value in
+                print("ReceivedValue:", value)
+            }
+            .store(in: &subscriptions)
+    }
+}
+
+let receiver3 = Receiver3()
+// Timerは一定期間ごとにconnetctメソッドを呼び出すことで、Publishをする
+// ※autoConnectでもOK
+//publisher2.connect()
+
+// Publisherはこれ以外にもURLSessionやSuquenceにも用意されている。またPublisherを自分で作ることもできる
+
